c342b3662c9be3e109e3dc7ca9f111c0e2ae343b
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-07 18:16:29 +0000

    pdo first steps

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 require 'functions.php';
 //require 'router.php';
 
-class Person
-{
-  public $name;
-  public $age;
+$prefix = 'mysql';
+$host = '127.0.0.1';
+$port = '3306';
+$dbname = 'myapp';
+$charset = 'latin1';
+$user = 'root';
+$password = 'mariadb';
 
-  function breathe(): void
-  {
-    echo "{$this->name} is breathing";
-  }
+$dsn = "{$prefix}:host={$host};port={$port};dbname={$dbname};charset={$charset}";
+$pdo = new PDO($dsn, $user, $password);
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>{$post['title']}</li>";
 }
-
-
-$person = new Person();
-$person->name = 'John Doe';
-$person->age = 25;
-
-dd($person->breathe());
