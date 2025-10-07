b8c9e21ab12ebc0949aaa03363e26b69f99d5f06
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-07 17:50:18 +0000

    pdo first steps

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+class Person
+{
+  public $name;
+  public $age;
+
+  function breathe(): void
+  {
+    echo "{$this->name} is breathing";
+  }
+}
+
+
+$person = new Person();
+$person->name = 'John Doe';
+$person->age = 25;
+
+dd($person->breathe());
