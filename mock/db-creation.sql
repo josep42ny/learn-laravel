ba992436639c23a3dd299c95f753f612d6cf97a6
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-10 08:48:40 +0000

    refactor: move sql creation script

--- mock/db-creation.sql
+++ mock/db-creation.sql
@@ -1,12 +1,26 @@
-drop database if exists myapp;
-create database myapp;
+create or replace database myapp character set utf8mb4 collate utf8mb4_general_ci;
 use myapp;
 
-create table posts (
-  id int unsigned not null auto_increment primary key,
-  title varchar(255) not null
-  );
+create table User (
+  id int unsigned auto_increment primary key,
+  firstName varchar(255) not null,
+  email varchar(255) not null unique
+);
 
-insert into posts(title) values 
-('My first blog post'),
-('My second blog post');
+create table Note (
+  id int unsigned auto_increment primary key,
+  body varchar(255) not null,
+  userId int unsigned not null,
+  constraint fk_Note_User foreign key (userId) references User(id)
+  on delete cascade on update cascade
+);
+
+insert into User (firstName, email) values
+('John', 'john@example.com'),
+('Kate', 'kate@example.com');
+
+insert into Note (userId, body) values
+(1, 'Ideas for next vacation'),
+(2, 'Next art project research'),
+(1, 'Work reminders'),
+(2, 'Design techniques blog post');
