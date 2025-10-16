d36bf7dfa659ea39bdd777e6f0a319764822fef8
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-16 11:05:22 +0000

    intro to forms and request methods

---
+++ routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  '/' => 'controllers/home.php',
+  '/notes' => 'controllers/notes.php',
+  '/note' => 'controllers/note.php',
+  '/notes/create' => 'controllers/notes-create.php',
+  '/contact' => 'controllers/contact.php',
+  '/about' => 'controllers/about.php',
+];
