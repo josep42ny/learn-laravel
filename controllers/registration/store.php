570efc24e27e45e155815577b1afa7639c5cfc8d
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-25 17:22:43 +0000

    hash 'em passwords

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -32,7 +32,7 @@ if ($result) {
 } else {
   $db->query('insert into User (email, password) values (:email, :password)', [
     'email' => $email,
-    'password' => $password
+    'password' => password_hash($password, PASSWORD_BCRYPT)
   ]);
 
   $_SESSION['user'] = [
