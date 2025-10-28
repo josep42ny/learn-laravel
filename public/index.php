b35d0b518a8529a1fcf14e8226b9e2600805776e
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-10-28 09:54:19 +0000

    composer and free autolading

--- public/index.php
+++ public/index.php
@@ -2,18 +2,12 @@
 
 use Core\Session;
 
-session_start();
-
 const BASE_PATH = __DIR__ . '/../';
-
+require BASE_PATH . 'vendor/autoload.php';
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-  $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-  require baseUrl($class . '.php');
-});
-
 require baseUrl('bootstrap.php');
+
+session_start();
 
 $router = new Core\Router();
 $routes = require baseUrl('routes.php');
