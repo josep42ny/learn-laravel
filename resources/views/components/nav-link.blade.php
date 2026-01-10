00041955a969ec5c521d3266f8f2872caf17a083
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-01-10 23:13:57 +0100

    feat: autoloading, namespaces, and models

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,4 @@
 @props(['active' => false, 'type' => 'link'])
 
-<{{ $type === 'button' ? 'button' : 'a' }} {{ $attributes}} aria-current="{{$active ? 'page' : 'false'}}"
-class="rounded-md px-3 py-2 text-sm font-medium {{$active ? 'bg-gray-900 dark:bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}}">{{$slot}}</{{ $type === 'button' ? 'button' : 'a' }}>
+<a {{ $attributes}} aria-current="{{$active ? 'page' : 'false'}}"
+   class="rounded-md px-3 py-2 text-sm font-medium {{$active ? 'bg-gray-900 dark:bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}}">{{$slot}}</a>
