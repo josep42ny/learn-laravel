ed9af3da8c6bd582511984fc5df8d87655c24691
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-01-10 21:39:10 +0100

    feat: style the currently active navigation link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,12 +21,10 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 dark:bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                            <a href="/" aria-current="page"
-                               class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white dark:bg-gray-950/50">Home</a>
-                            <a href="/about"
-                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                            <a href="/contact"
-                               class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                            <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+                            <x-nav-link href="/contact" :active="request()->is('contact')" type="button">Contact
+                            </x-nav-link>
                         </div>
                     </div>
                 </div>
