270762335cfe5ff7e85e7376d79e6433afcaec2d
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-01-10 21:03:20 +0100

    feat: create a layout file using Laravel components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
+    <title>Home - Learn</title>
 </head>
 <body>
-<h1>About page</h1>
+<nav>
+    <x-nav-link href="/">Home</x-nav-link>
+    <x-nav-link href="/about">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
+</nav>
+
+{{$slot}}
 </body>
 </html>
